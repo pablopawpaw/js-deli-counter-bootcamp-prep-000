7356b9fc199cc9d59ce6b3e4d24f349b481e7f4b
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-26 00:43:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ function currentLine(line) {
     for (var i=0;i<line.length;i++) {
       currLineStatement = currLineStatement + (i+1) + ". " + line[i] +
         if(i===line.length-1) { 
-          
+          currLineStatement
         }
       ", "
-      currLineStatement
+      
       
     }
     return currLineStatement
